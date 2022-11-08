ff3d0ebc2751c6d2602d0d32c6654c57f7d2285b
FelipeRiv <luisriv_@outlook.com>
2022-11-08 17:32:52 -0600

    29 inicio del proyecto seccion 4

---
+++ 03-bases-node/app.js
@@ -0,0 +1,13 @@
+const base = 5
+const max = 10
+
+console.clear();
+console.log('========================');
+console.log(`       Tabla del ${base}`);
+console.log('========================');
+
+for (let index = 1; index <= max; index++) {
+
+    console.log(`${base} X ${index} = ${base * index}`);
+    
+}
