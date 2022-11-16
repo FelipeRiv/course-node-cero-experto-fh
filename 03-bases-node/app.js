a1e9a4cd0ee20d2ef6bad6451918a3fd8e3b12a2
FelipeRiv <luisriv_@outlook.com>
2022-11-15 19:02:32 -0600

    30 requerir paquetes - require

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,13 +1,30 @@
-const base = 5
+const fs = require('fs')
+
+const base = 3
 const max = 10
 
+let output = ''
+
 console.clear();
+
 console.log('========================');
 console.log(`       Tabla del ${base}`);
 console.log('========================');
 
 for (let index = 1; index <= max; index++) {
 
-    console.log(`${base} X ${index} = ${base * index}`);
+    // console.log(`${base} X ${index} = ${base * index}`);
+    output += `${base} X ${index} = ${base * index}\n`
     
 }
+
+console.log(output);
+
+fs.writeFile( 'tabla-'+base+'.txt', output, (err) => {
+
+    if (err) throw err;
+
+    console.log('tabla-'+base+'.txt - created!');
+
+
+})
