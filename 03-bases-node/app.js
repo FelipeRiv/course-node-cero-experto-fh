e45a3744ae4d744fd0715349c7e4acf30660e377
FelipeRiv <luisriv_@outlook.com>
2022-11-16 22:25:56 -0600

    32 recibir informacion desde linea de comando

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -3,12 +3,29 @@ const {createFile, createFileSync} = require('./helpers/multiplication')
 
 console.clear();
 
+// * array: arguments will be:
+// * 0: global path where node app is installed
+// * 1:  path where the current app is installed
+// * 2:  my arg base for eg
+console.log(process.argv); // :array
+console.log(process.argv[2]);
+
+// const [ globaPath, appPath, base ] = process.argv
+const [ , , baseArg = 'base=5' ] = process.argv
+
+const [ , base] = baseArg.split('=')
+console.log("🚀 ~ file: app.js ~ line 14 ~ base:", base)
+
+
+// * there a package for handle console args -> yargs 
+
+// * node --help
 
 createFile(7)
     .then( (fileName) => console.log(fileName) )
     .catch( (err) => console.log(err) )
 
-createFileSync(9)
+createFileSync(base)
     .then( (fileName) => console.log(fileName) )
     .catch( (err) => console.log(err) )
 
